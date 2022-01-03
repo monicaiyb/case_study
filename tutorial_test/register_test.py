83642fd5b3cac810b2e44c43a372de45f2a8dba8
monicaiyb <monicaiyb@gmail.com>
2022-01-03 18:43:40 +0300

    change api test

--- tutorial_test/register_test.py
+++ tutorial_test/register_test.py
@@ -1,16 +1,8 @@
 import pytest
 import requests
 import json
-@pytest.mark.parametrize("id", "name","phone","password","email",[(1,"George","077125555","husyd"),(2,"Janet")])
-def test_list_valid_user(url,id,name):
-	url = "http://localhost:3000/register"
-	resp = requests.get(url)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert j['data']['id'] == id, resp.text
-	assert j['data']['first_name'] == name, resp.text
 
-def test_list_invaliduser(url):
-	url = "http://localhost:3000/register"
-	resp = requests.get(url)
-	assert resp.status_code == 404, resp.text
+
+def test_get_home():
+    response = requests.get("http://localkost:3000/register")
+    assert response.headers["Content-Type"] == "application/json"
